fe78979f54d2104778a48663ec1b53618277dd01
Henry Brophy <henrybrophy93@gmail.com>
2020-10-11 11:13:20 -0400

    Completed word_count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,29 @@
-def word_count(s):
-    # Your code here
 
+def word_count(s):
+
+    # Your code here
+    counts = {}
+    newWords = []
+
+    split = s.lower().split()
+    print(split)
+
+    for word in split:
+        newWords.append(word.replace(':', '').replace(';', '').replace(',', '').replace('.', '').replace('-', '').replace('+', '').replace('=', '').replace('/', '').replace('|', '').replace(
+            '[', '').replace(']', '').replace('{', '').replace('}', '').replace('(', '').replace(')', '').replace('*', '').replace('^', '').replace('&', '').replace('"', '').replace('\\', ''))
+
+    if newWords == [""]:
+        return counts
+
+    for words in newWords:
+        counts[words] = newWords.count(words)
+
+    return counts
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
